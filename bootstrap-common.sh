d5108491a068d33ae8a31eec7abdcbec762c4074
Shahbaz Chaudhary <shahbazc@gmail.com>
2016-10-28 15:18:41 -0500

    fixes proxy string

--- bootstrap-common.sh
+++ bootstrap-common.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'Acquire::http { Proxy "http:/172.28.128.2:3142"; };' > /etc/apt/apt.conf.d/02proxy
+echo 'Acquire::http::Proxy "http://172.28.128.2:3142";' > /etc/apt/apt.conf.d/02proxy
 
 #sudo apt -y install python-minimal
 sudo apt -y install openjdk-8-jdk-headless
